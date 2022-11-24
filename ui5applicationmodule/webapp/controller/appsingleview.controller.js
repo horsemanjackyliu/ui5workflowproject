6f1ef4343daa7f8995709e91bb2d1e6dbec8531d
Jacky Liu <jacky.liu02@sap.com>
2022-11-24 06:23:35 +0000

    revise controller

--- ui5applicationmodule/webapp/controller/appsingleview.controller.js
+++ ui5applicationmodule/webapp/controller/appsingleview.controller.js
@@ -13,19 +13,7 @@ sap.ui.define([
         return Controller.extend("ui5applicationmodule.controller.appsingleview", {
             onInit: function () {
 
-
             },
-            // _openPdfViewerForSource: function (pdfDocumentURL) {
-            //     if (!this._pdfViewer) {
-            //         this._pdfViewer = new PDFViewer();
-            //         // this.getFragment().addDependent(this._pdfViewer);
-            //         this._pdfViewer.attachError(event => ErrorHandlerSingleton.getInstance().onError(event));
-            //         // to show the pdf in this viewer, need to whitelist the blob protocol
-            //         URLWhitelist.add("blob");
-            //     }
-            //     this._pdfViewer.setSource(pdfDocumentURL);
-            //     this._pdfViewer.open();
-            // },
             pdfRender:function(){
 
                 var printd = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><form1><LabelForm><DeliveryId>Mirum est ut animus agitatione motuque corporis excitetut.</DeliveryId><Position>Ego ille</Position><MaterialNo>Si manu vacuas</MaterialNo><Quantity>Apros tres et quidem</Quantity><Package>Mirum est</Package><QRCode>01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789</QRCode></LabelForm><LabelForm><DeliveryId>Ad retia sedebam: erat in proximo non venabulum aut lancea, sed stilus et pugilares:</DeliveryId><Position>Licebit auctore</Position><MaterialNo>Proinde</MaterialNo><Quantity>Am undique</Quantity><Package>Ad retia sedebam</Package><QRCode>01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789</QRCode></LabelForm><LabelForm><DeliveryId>meditabar aliquid enotabamque, ut, si manus vacuas, plenas tamen ceras reportarem.</DeliveryId><Position>Vale</Position><MaterialNo>Ego ille</MaterialNo><Quantity>Si manu vacuas</Quantity><Package>Apros tres et quidem</Package><QRCode>01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789</QRCode></LabelForm></form1>";
@@ -72,11 +60,7 @@ sap.ui.define([
                                  this._pdfViewer.setSource(pdfDocumentURL);
                                  this._pdfViewer.open(); 
 
-
-
-                        // this._openPdfViewerForSource(pdfDocumentURL);
-                       
-
+                    
                     },
                     error: function (err) {
                         console.log(err);
@@ -84,44 +68,6 @@ sap.ui.define([
 
                 } });
 
-
-            },
-            pressButton: function () {             
-                var sEmployee = this.getView().byId("employeeInput").getValue();
-                var sITequipment = this.getView().byId("itequipmentInput").getValue();
-                var sQuantity = parseInt(this.getView().byId("quantityInput").getValue());
-
-                var startContext = {employee: sEmployee, itequipment: sITequipment, quantity: sQuantity}; 
-                var workflowStartPayload = {definitionId: "myui5workflow", context: startContext}
-
-                $.ajax({
-                    url: "/ui5applicationmodule/bpmworkflowruntime/v1/xsrf-token",
-                    method: "GET",
-                    headers: {
-                        "X-CSRF-Token": "Fetch"
-                    },
-                    success: function (result, xhr, data) {
-                        var token = data.getResponseHeader("X-CSRF-Token");
-                        if (token === null) return;
-                    
-                        $.ajax({
-                            url: "/ui5applicationmodule/bpmworkflowruntime/v1/workflow-instances",
-                            type: "POST",
-                            data: JSON.stringify(workflowStartPayload),
-                            headers: {
-                                "X-CSRF-Token": token,
-                                "Content-Type": "application/json"
-                            },
-                            async: false,
-                            success: function (data) {
-                                MessageBox.information("The workflow has successfully started");
-                            },
-                            error: function (data) {
-
-                            }
-                        });
-                    }
-                });
 
             }
 
